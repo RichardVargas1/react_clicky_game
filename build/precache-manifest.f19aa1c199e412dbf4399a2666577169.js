2832eb0ce1a8dad8fc22b0f80053100a44c2aa7d
RichardVargas1 <richard.vargas95@gmail.com>
2019-01-08 23:51:37 -0600

    BUILD

---
+++ build/precache-manifest.f19aa1c199e412dbf4399a2666577169.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "28b6cd7f48df24d7d9735dbed162023e",
+    "url": "/RichardVargas1/FuturamaPatternGame/static/media/hermes.28b6cd7f.png"
+  },
+  {
+    "revision": "047b3b575b26f9c99311",
+    "url": "/RichardVargas1/FuturamaPatternGame/static/css/main.907cea02.chunk.css"
+  },
+  {
+    "revision": "de822213a7d64472361b",
+    "url": "/RichardVargas1/FuturamaPatternGame/static/js/1.de822213.chunk.js"
+  },
+  {
+    "revision": "9b98b22a3f72fc2c6363",
+    "url": "/RichardVargas1/FuturamaPatternGame/static/js/runtime~main.9b98b22a.js"
+  },
+  {
+    "revision": "26903f44d5e217b2912a97aa81bf6f1f",
+    "url": "/RichardVargas1/FuturamaPatternGame/static/media/FuturamaAmyWong.26903f44.png"
+  },
+  {
+    "revision": "574b0fdf37806b4f7f89c57fe895506d",
+    "url": "/RichardVargas1/FuturamaPatternGame/static/media/bender.574b0fdf.png"
+  },
+  {
+    "revision": "c008528cf3c8bd9a7a61a59b050f1338",
+    "url": "/RichardVargas1/FuturamaPatternGame/static/media/farnsworth.c008528c.png"
+  },
+  {
+    "revision": "086a9236b26e2604828958fe79868b0a",
+    "url": "/RichardVargas1/FuturamaPatternGame/static/media/fry.086a9236.png"
+  },
+  {
+    "revision": "047b3b575b26f9c99311",
+    "url": "/RichardVargas1/FuturamaPatternGame/static/js/main.047b3b57.chunk.js"
+  },
+  {
+    "revision": "f81cb7f03219e6e02de2a3daac8217d9",
+    "url": "/RichardVargas1/FuturamaPatternGame/static/media/leela.f81cb7f0.png"
+  },
+  {
+    "revision": "6f43e8bd9e95177a97bdf5656f8ee013",
+    "url": "/RichardVargas1/FuturamaPatternGame/static/media/_lrrr.6f43e8bd.jpg"
+  },
+  {
+    "revision": "787b897d0d36107e5677ada69670c305",
+    "url": "/RichardVargas1/FuturamaPatternGame/static/media/nibbler.787b897d.jpg"
+  },
+  {
+    "revision": "10a94e2075bbe4feeea2d837c0940d94",
+    "url": "/RichardVargas1/FuturamaPatternGame/static/media/scruffy.10a94e20.png"
+  },
+  {
+    "revision": "59a85e73f4fa957e76cb0a86a2a48f9c",
+    "url": "/RichardVargas1/FuturamaPatternGame/static/media/zapp.59a85e73.png"
+  },
+  {
+    "revision": "8f88cd0f84b66d1541cc9c4e88baaeb9",
+    "url": "/RichardVargas1/FuturamaPatternGame/static/media/zoidberg.8f88cd0f.png"
+  },
+  {
+    "revision": "90491eedf74d76df45859ce3f1b95b07",
+    "url": "/RichardVargas1/FuturamaPatternGame/index.html"
+  }
+];
